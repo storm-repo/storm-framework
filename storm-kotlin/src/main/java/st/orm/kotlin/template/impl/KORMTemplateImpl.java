db781bd27893f9261fb330343f83b30630c42175
Leon van Zantvoort <zantvoort@beatgridmedia.com>
2025-03-04 20:58:17 +0100

    Update copyright notice. Relates to #5

--- storm-kotlin/src/main/java/st/orm/kotlin/template/impl/KORMTemplateImpl.java
+++ storm-kotlin/src/main/java/st/orm/kotlin/template/impl/KORMTemplateImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 - 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package st.orm.kotlin.template.impl;
 
 import jakarta.annotation.Nonnull;
@@ -15,7 +30,6 @@ import st.orm.spi.ORMReflection;
 import st.orm.spi.Providers;
 import st.orm.template.ORMTemplate;
 import st.orm.template.Sql;
-import st.orm.template.SqlInterceptor;
 import st.orm.template.SqlTemplateException;
 
 import java.lang.reflect.InvocationTargetException;
