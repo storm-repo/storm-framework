45d2736fdf33b05a3041e114b84f7ef113d2ab84
Leon van Zantvoort <zantvoort@beatgridmedia.com>
2025-11-02 20:40:40 +0100

    Move @Json to foundation package. Relates to #45

--- storm-json/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module storm.json {
-    requires storm.foundation;
-    requires storm.core;
-    requires jakarta.annotation;
-    requires java.sql;
-    requires com.fasterxml.jackson.databind;
-    provides st.orm.core.spi.ORMConverterProvider with st.orm.json.spi.JsonORMConverterProviderImpl;
-}
--- storm-json/src/test/java/st/orm/json/model/VetSpecialtyPK.java
+++
@@ -1,9 +0,0 @@
-package st.orm.json.model;
-
-import lombok.Builder;
-
-@Builder(toBuilder = true)
-public record VetSpecialtyPK(
-        int vetId,
-        int specialtyId
-) {}
