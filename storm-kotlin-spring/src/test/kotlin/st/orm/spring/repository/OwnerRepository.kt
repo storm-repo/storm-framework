112fb1632ac35f887c7e182ed09429882526fbb2
Leon van Zantvoort <zantvoort@beatgridmedia.com>
2025-11-30 15:57:58 +0100

    Replace Storm's @NoRepositoryBean to Spring Frameworks variant. Relates to #45

--- storm-kotlin-spring/src/test/kotlin/st/orm/spring/repository/OwnerRepository.kt
+++ storm-kotlin-spring/src/test/kotlin/st/orm/spring/repository/OwnerRepository.kt
@@ -1,7 +1,7 @@
 package st.orm.spring.repository
 
+import org.springframework.data.repository.NoRepositoryBean
 import st.orm.repository.EntityRepository
-import st.orm.spring.NoRepositoryBean
 import st.orm.spring.model.Owner
 
 @NoRepositoryBean
