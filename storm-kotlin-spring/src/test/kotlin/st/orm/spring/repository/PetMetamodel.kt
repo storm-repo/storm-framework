6b703d3d93fa556a7918f78a7281c4580a4c35c8
Leon van Zantvoort <zantvoort@beatgridmedia.com>
2026-08-12 09:51:42 +0200

    docs: state why PetMetamodel has no code references

--- storm-kotlin-spring/src/test/kotlin/st/orm/spring/repository/PetMetamodel.kt
+++ storm-kotlin-spring/src/test/kotlin/st/orm/spring/repository/PetMetamodel.kt
@@ -5,5 +5,8 @@ import st.orm.Metamodel
 /**
  * Simulates a generated metamodel interface in the same package as repositories.
  * This should NOT be picked up by [st.orm.spring.RepositoryBeanFactoryPostProcessor].
+ *
+ * Deliberately has no code references: it is exercised through classpath scanning, and
+ * `RepositoryTest` asserts by bean name that no bean was registered for it.
  */
 internal interface PetMetamodel : Metamodel<st.orm.spring.model.Pet, st.orm.spring.model.Pet>
